ffe53bf453f92b96a51f280dc7761dab185d61b0
scottwcode <scottmail77@gmail.com>
2022-12-08 17:06:50 -0500

    code to check room availability by roomID, or all rooms available if needed

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/scottwcode/go-bnb-bookings/internal/models"
+import (
+	"time"
+
+	"github.com/scottwcode/go-bnb-bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
