694ba844d950ae1cf984606bda1a6ab0e96161c0
scottwcode <scottmail77@gmail.com>
2022-12-13 13:07:21 -0500

    Some code cleanup and added seed data when needing to refresh rooms and restrictions tables

---
+++ migrations/20221213173501_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.rooms
+  (room_name,created_at,updated_at)
+VALUES
+  ('Generals Quarters', '2022-12-18 00:00:00.000', '2022-12-20 00:00:00.000'),
+  ('Majors Suite', '2022-12-08 00:00:00.000', '2022-12-08 00:00:00.000');
---
+++ migrations/20221213175020_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.restrictions
+  (restriction_name,created_at,updated_at)
+VALUES
+  ('Reservation', '2022-12-18 00:00:00.000', '2022-12-18 00:00:00.000'),
+  ('Owner Block', '2020-11-19 00:00:00.000', '2020-11-19 00:00:00.000');
