caee3e6dfd7b7f564ae32f51635fd6df2fe5d758
scottwcode <scottmail77@gmail.com>
2022-12-22 14:08:14 -0500

    Added migration files to seed users

---
+++ migrations/20221222185045_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."users"
+  ("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+  ('Scott', 'W', 'admin@admin.com', '$2a$12$Wm8SHtNb7v9oRF6Rmpp/c.PHE5tERA6mAfvShxcWJWT7i5nwXg94i', 3, '2020-12-04 00:00:00', '2020-12-04 00:00:00')
